292cd475999aa1878c2624cf375132bebdbc8566
Ishmael Hike <ishmaeliabaya@gmail.com>
2025-10-15 11:44:19 +0300

    backend url update

---
+++ client/src/services/backenInt.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { io } from "socket.io-client";
+
+
+const BackendBaseUrl = "https://groupchat-plp.onrender.com";
+const APIBaseUrl = "https://groupchat-plp.onrender.com/api";
+
+const API = axios.create({
+    baseURL: APIBaseUrl,
+});
+
+
+export const registerUser = ( username )=> API.post("/auth/register", { username });
+
+export const getRooms = ()=> API.get("/rooms");
+export const createRoom = (name) => API.post("/rooms", { name });
+
+export const getMessages = (roomId) => API.get(`/messages/${roomId}`);
+
+
+export const socket = io(BackendBaseUrl, { autoConnect: false });
